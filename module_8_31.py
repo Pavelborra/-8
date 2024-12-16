98e5255bff8965f41acd68e2c5a5988a9f12edd5
Pavelborra <borra2024pavel@gmail.com>
2024-12-16 23:31:42 +0300

    Add files via upload

---
+++ module_8_31.py
@@ -0,0 +1,73 @@
+# Определяем пользовательское исключение для некорректного VIN номера
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message  # Сообщение об ошибке
+
+
+# Определяем пользовательское исключение для некорректных номеров автомобиля
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message  # Сообщение об ошибке
+
+
+# Определяем класс Car
+class Car:
+    def __init__(self, model, vin, numbers):
+        self.model = model  # Название автомобиля (публичный атрибут)
+        if self.__is_valid_vin(vin):
+            self.__vin = vin
+        if self.__is_valid_numbers(numbers):
+            self.__numbers = numbers  # Номера автомобиля (приватный атрибут)
+
+
+
+    # Приватный метод для проверки корректности VIN номера
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):  # Проверка на целое число
+            raise IncorrectVinNumber('Некорректный тип vin номер')  # Выбрасываем исключение, если тип некорректен
+
+        if vin_number < 1000000 or vin_number > 9999999:  # Проверка на диапазон
+            raise IncorrectVinNumber(
+                'Неверный диапазон для vin номера')  # Выбрасываем исключение, если номер вне диапазона
+
+        return True  # Если проверки пройдены, возвращаем True
+
+    # Приватный метод для проверки корректности номера автомобиля
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):  # Проверка на строку
+            raise IncorrectCarNumbers(
+                'Некорректный тип данных для номеров')  # Выбрасываем исключение, если тип некорректен
+
+        if len(numbers) != 6:  # Проверка на длину строки
+            raise IncorrectCarNumbers('Неверная длина номера')  # Выбрасываем исключение, если длина некорректна
+
+        return True  # Если проверки пройдены, возвращаем True
+
+
+# Примеры использования классов и обработки исключений
+try:
+    first = Car('Model1', 1000000, 'f123dj')  # Создаем объект Car с корректными данными
+except IncorrectVinNumber as exc:
+    print(exc.message)  # Если возникло исключение IncorrectVinNumber, выводим сообщение об ошибке
+except IncorrectCarNumbers as exc:
+    print(exc.message)  # Если возникло исключение IncorrectCarNumbers, выводим сообщение об ошибке
+else:
+    print(f'{first.model} успешно создан')  # Если объект создан успешно, выводим сообщение
+
+try:
+    second = Car('Model2', 300, 'т001тр')  # Создаем объект Car с некорректным VIN номером
+except IncorrectVinNumber as exc:
+    print(exc.message)  # Выводим сообщение об ошибке для некорректного VIN номера
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')  # Создаем объект Car с некорректными номерами
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
